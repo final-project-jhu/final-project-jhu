ddfdc18853282afdc158ec526e7a589beebe2833
Cory Piette <coryjpiette@gmail.com>
2021-10-22 16:05:01 -0400

    working on server and pages

---
+++ client/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginForm from "../components/Login";
+import { Container } from "../components/Grid";
+
+const Login = () => {
+    return (
+        <Container fluid>
+            <LoginForm />
+        </Container>
+    );
+};
+
+export default Login;
