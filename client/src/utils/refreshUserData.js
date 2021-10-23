b658e69dda1817745c8c1fd54bde9660b3ecd06d
Cory Piette <coryjpiette@gmail.com>
2021-10-23 18:05:23 -0400

    adding update/refresh team data and code assign

--- client/src/utils/refreshUserData.js
+++ client/src/utils/refreshUserData.js
@@ -8,3 +8,46 @@ import {
 } from './actions';
 
 
+function refreshUserData(dispatch) {
+    
+    API.getUserData()
+      .then(response => {
+        dispatch({
+          type: UPDATE_USERNAME,
+          username: response.data.name,
+          userId: response.data.id
+        });
+      })
+      .catch(err => {
+         console.log(err);
+        dispatch({ type: UPDATE_USERNAME, username: "", userId: null });
+      })
+  
+
+      API.getTeamInfo()
+
+      .then(response => {
+        console.log(response);
+         dispatch({
+            type: UPDATE_TEAM,
+          team: response.data.name,
+          inviteCode: response.data.invite_code,
+        });
+        dispatch({
+              type: UPDATE_MEMBERS,
+          members: response.data.members,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+          dispatch({
+            type: UPDATE_TEAM,
+           team: null,
+          inviteCode: null,
+        });
+          dispatch({
+            type: UPDATE_MEMBERS,
+
+          members: [],
+        });
+      })
