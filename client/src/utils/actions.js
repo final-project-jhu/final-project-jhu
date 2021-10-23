2ba6d3f461c7a8d8dd70bd9eb909a8551820c330
Cory Piette <coryjpiette@gmail.com>
2021-10-23 17:51:49 -0400

    adding action to link with alternate GlobalState

---
+++ client/src/utils/actions.js
@@ -0,0 +1,13 @@
+
+export const UPDATE_USERNAME = "UPDATE_USERNAME";
+export const UPDATE_TEAM = "UPDATE_TEAM";
+export const UPDATE_MEMBERS = "UPDATE_MEMBERS";
+
+export const UPDATE_TASKS = "UPDATE_TASKS"; 
+export const UPDATE_TASKS = "UPDATE_TASKS"; 
+export const ADD_TASK = "ADD_TASK"; 
+export const REMOVE_TASK = "REMOVE_TASK"; 
+
+export const UPDATE_ATTEMPTS = "UPDATE_ATTEMPTS";
+export const COMPLETE_ATTEMPT = "COMPLETE_ATTEMPT"; 
+export const UNDO_ATTEMPT = "UNDO_ATTEMPT"; 
