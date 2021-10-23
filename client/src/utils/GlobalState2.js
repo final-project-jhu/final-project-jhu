15b1aac275564687dbc894125c005ecc5c218daf
Cory Piette <coryjpiette@gmail.com>
2021-10-23 16:45:35 -0400

    finishing alternate GlobalState2

--- client/src/utils/GlobalState2.js
+++ client/src/utils/GlobalState2.js
@@ -38,3 +38,76 @@ const reducer = (state, action) => {
                         members: action.members
                     };
                 
+
+                    case UPDATE_TASKS:
+            return {
+                ...state,
+                chores: action.tasks
+            };
+        
+        case UPDATE_TASK:
+            return {
+                ...state,
+                chores: state.tasks.map(task => {
+                    return (tasks.id !== action.id ? chore : action.tasks);
+                })
+            };
+
+        case ADD_TASK:
+            return {
+                ...state,
+                task: [...state.chores, action.task],
+            };
+            
+        case REMOVE_TASK:
+            return {
+                ...state,
+                tasks: state.chores.filter(task => {
+                    return task.id !== action.id;
+                })
+            };
+
+        case UPDATE_REPETITIONS:
+            return {
+                ...state,
+                repetitions: action.repetitions
+            };
+
+        case COMPLETE_ATTEMPT:
+            return {
+                ...state,
+                attempts: state.attempts.map(attempt => {
+                    return (attempt.id !== action.id ? attempts: {...attempt, complete:true});
+                })
+            };
+
+           case UNDO_ATTEMPT:
+            return {
+                ...state,
+                attempts: state.attempts.map(attempt => {
+                    return (attempt.id !== action.id ? attempt : {...attempt, complete:false});
+                })
+            }
+
+               default:
+                return state;
+            
+    }
+}
+
+   const TaskProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useReducer(reducer, {
+        members: [],
+        tasks: [],
+        attempts: []
+
+    });
+
+      return <Provider value={[state, dispatch]} {...props} />
+};
+
+const useTaskContext = () => {
+    return useContext(TaskContext);
+};
+
+export { TaskProvider, useTaskContext };
