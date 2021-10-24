25b0d7399c4482ab06785529e6d778e23387529c
Cory Piette <coryjpiette@gmail.com>
2021-10-24 19:21:53 -0400

    working on Join btn

--- client/src/components/Join/index.js
+++ client/src/components/Join/index.js
@@ -5,3 +5,21 @@ import ErrorMessage from "../ErrorMessage";
 import { useChoreContext } from "../../utils/GlobalState";
 import API from '../../utils/API';
 import refreshUserData from "../../utils/refreshUserData";
+
+function JoinBtn() {
+    
+    const [isOpen, setIsOpen] = useState(false);
+    const dispatch = useTaskContext()[1];
+  
+    const [error, setError] = useState(null);
+  
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => setIsOpen(false);
+
+    const inviteRef = useRef();
+    const handleClick = () => {
+      if (!inviteRef.current.value) {
+        setError("Please enter your team invite code.");
+        return;
+      }
+  
